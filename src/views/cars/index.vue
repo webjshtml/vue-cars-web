82b82959faac1e7cb789b85b67d4bf8b8e5f98be
bigbigtime <409019683@qq.com>
2020-06-15 23:17:52 +0800

    cars

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -8,8 +8,36 @@
                 </h4>
                 <p class="cars-attr">新能源汽车  5座</p>
             </header>
-            <div class="cars-content"></div>
-            <footer></footer>
+            <div class="cars-content">
+                <div class="info">
+                    <div>
+                        <h4 class="cars-number">粤 B745N8</h4>
+                        <div>
+                            <ul class="cars-electric active-li-8">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <p class="distance">
+                                <sub>约</sub>
+                                <strong>600</strong>
+                                <sub>KM</sub>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <img src="../../assets/images/pic001.jpg" alt="">
+            </div>
+            <footer>
+                <a href="javascript: void(0);" class="parking-link">某某停车场</a>
+            </footer>
         </section>
         <span @click="user">汽车列表</span>
     </div>
