f29dd8dd0b938e7f2a9be52175dbedd6996f78cc
bigbigtime <409019683@qq.com>
2020-06-16 22:37:06 +0800

    lesson15

---
+++ src/components/carsList/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="cars-item">
+        <header>
+            <h4 class="cars-logo">
+                <img src="../../assets/images/cars-logo.png" alt="Mustang 2019款">
+                <span class="name">Mustang 2019款</span>
+            </h4>
+            <p class="cars-attr">新能源汽车  5座</p>
+        </header>
+        <div class="cars-content">
+            <div class="info">
+                <div>
+                    <h4 class="cars-number">粤 B745N8</h4>
+                    <div>
+                        <ul class="cars-electric active-li-8">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <p class="distance">
+                            <sub>约</sub>
+                            <strong>600</strong>
+                            <sub>KM</sub>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <img src="../../assets/images/pic001.jpg" alt="">
+        </div>
+        <footer>
+            <a href="javascript: void(0);" class="parking-link">某某停车场</a>
+        </footer>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -1,52 +1,37 @@
 <template>
-    <div class="index-wrap">
-        <section class="cars-item">
-            <header>
-                <h4 class="cars-logo">
-                    <img src="../../assets/images/cars-logo.png" alt="Mustang 2019款">
-                    <span class="name">Mustang 2019款</span>
-                </h4>
-                <p class="cars-attr">新能源汽车  5座</p>
-            </header>
-            <div class="cars-content">
-                <div class="info">
-                    <div>
-                        <h4 class="cars-number">粤 B745N8</h4>
-                        <div>
-                            <ul class="cars-electric active-li-8">
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                            </ul>
-                            <p class="distance">
-                                <sub>约</sub>
-                                <strong>600</strong>
-                                <sub>KM</sub>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <img src="../../assets/images/pic001.jpg" alt="">
-            </div>
-            <footer>
-                <a href="javascript: void(0);" class="parking-link">某某停车场</a>
-            </footer>
-        </section>
-        <span @click="user">汽车列表</span>
+    <div class="cars-wrap">
+        <div class="cars-swiper-wrap">
+            <swiper class="swiper" :options="swiperOption">
+                <swiper-slide><CarsItem /></swiper-slide>
+                <swiper-slide><CarsItem /></swiper-slide>
+                <swiper-slide><CarsItem /></swiper-slide>
+                <swiper-slide><CarsItem /></swiper-slide>
+                <swiper-slide><CarsItem /></swiper-slide>
+            </swiper>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+        </div>
     </div>
 </template>
 <script>
+// cars item
+import CarsItem from "@c/carsList";
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 export default {
     name: "Cars",
+    components: { CarsItem, Swiper, SwiperSlide },
     data(){
-        return {}
+        return {
+            swiperOption: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            }
+        }
     },
     methods: {
         user(){
